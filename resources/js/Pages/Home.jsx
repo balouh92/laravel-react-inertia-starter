e1b043bab5b89afe2f2b1bfbcf840b734445623d
Mirco <mirco.pieper92@gmail.com>
2026-03-16 12:41:17 +0100

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home({ message }) {
+    return (
+        <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>Laravel 12 + React + Inertia</h1>
+            <p>{message}</p>
+        </main>
+    )
+}
