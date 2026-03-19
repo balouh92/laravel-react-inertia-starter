7486007ddc12d73e03e57a1eb51a39dda9c6e48c
Mirco <mirco.pieper92@gmail.com>
2026-03-19 10:23:39 +0100

    chore: added default Layout for all pages

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { createInertiaApp } from '@inertiajs/react'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import {createRoot} from 'react-dom/client'
+import {createInertiaApp} from '@inertiajs/react'
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import AppLayout from "./Layouts/AppLayout.jsx";
 
 createInertiaApp({
-    resolve: (name) =>
-        resolvePageComponent(
+    resolve: async (name) => {
+        const page = await resolvePageComponent(
             `./Pages/${name}.jsx`,
             import.meta.glob('./Pages/**/*.jsx')
-        ),
-    setup({ el, App, props }) {
+        )
+        page.default.layout ??= (page) => <AppLayout>{page}</AppLayout>
+        return page;
+    },
+    setup({el, App, props}) {
         createRoot(el).render(<App {...props} />)
     },
     progress: {
         delay: 250,
-        color: '#4B5563',
-        includeCSS: true,
-        showSpinner: false,
-    },
+        color:
+            '#4B5563',
+        includeCSS:
+            true,
+        showSpinner:
+            false,
+    }
+    ,
 })
